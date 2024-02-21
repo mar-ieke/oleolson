5e9d3689299e6af639c33e37116cd04d000d6b2e
mar-ieke <marieke.witt@gmail.com>
2024-02-21 23:49:19 +0100

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
+import { initializeApp } from "firebase/app";
+import "firebase/database";
 
-function App() {
+const firebaseConfig = {
+  apiKey: "AIzaSyCrsuJ5CE-JKPWIJ87X7sHb-IElJf6x-qI",
+  authDomain: "exitgame-ef689.firebaseapp.com",
+  projectId: "exitgame-ef689",
+  storageBucket: "exitgame-ef689.appspot.com",
+  messagingSenderId: "678409964473",
+  appId: "1:678409964473:web:4595f0853f6ed48ea9c468",
+  measurementId: "G-WYD2BKKCQL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Access the database module from firebase
+const db = app.database();
+const usersRef = db.ref("activeUsers");
+
+const App = () => {
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [passwords, setPasswords] = useState({ first: "test", second: "test" });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [onlineUsers, setOnlineUsers] = useState(0);
+
+  useEffect(() => {
+    // Increment active user count
+    const onConnect = usersRef.transaction(
+      (currentUsers) => (currentUsers || 0) + 1
+    );
+
+    // Decrement on disconnect or unmount
+    const onDisconnect = () => {
+      usersRef.transaction((currentUsers) => (currentUsers || 1) - 1);
+    };
+    window.addEventListener("beforeunload", onDisconnect);
+
+    return () => {
+      onDisconnect();
+      window.removeEventListener("beforeunload", onDisconnect);
+    };
+  }, []);
+
+  const handleLogin = () => {
+    if (password1 === passwords.first && password2 === passwords.second) {
+      setIsLoggedIn(true);
+      localStorage.setItem("lastLogin", Date.now());
+    } else {
+      alert("Falsches Passwort");
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("lastLogin");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!isLoggedIn ? (
+        <div>
+          <p>
+            Nicht schlecht, ihr Flachwasserpfeifen seid wahrlich clever! Doch
+            wie steht es um den Teamspirit der Crew? Piratinnen, Piraten,
+            Meerjungfrauen und Wassermänner, nur wo ihr getrennt seid, werdet
+            ihr zusammen weiterkommen! {" "}
+          </p>
+          <br />
+          <input
+            type="password"
+            placeholder="Erstes Passwort"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Zweites Passwort"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+          <br />
+          <button onClick={handleLogin}>Abtauchen!</button>
+        </div>
+      ) : (
+        <div>
+          <p>{onlineUsers}</p>
+          {onlineUsers >= 2 && <Confetti />}
+          {onlineUsers >= 2 && (
+            <p>
+              Luftballons können kaputt gehen...{" "}
+            </p>
+          )}
+
+          <button onClick={handleLogout}>Ausloggen</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
